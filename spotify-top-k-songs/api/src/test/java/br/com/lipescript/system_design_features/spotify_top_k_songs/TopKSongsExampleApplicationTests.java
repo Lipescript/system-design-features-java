464ff667622644ce22371be702b69a287ee8d750
Lipescript <felipe.m199@gmail.com>
2025-11-05 11:40:42 -0300

    maven init

---
+++ spotify-top-k-songs/api/src/test/java/br/com/lipescript/system_design_features/spotify_top_k_songs/TopKSongsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lipescript.system_design_features.spotify_top_k_songs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopKSongsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spotify-top-k-songs/consumer/src/main/java/br/com/lipescript/system_design_features/spotify_top_k_songs/TopKSongsExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.lipescript.system_design_features.spotify_top_k_songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopKSongsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopKSongsExampleApplication.class, args);
+	}
+
+}
